64c46bd6cf7de5a88da33d3a51cf45d3f45f59e2
MinSeok Oh <minseok.dev@gmail.com>
2025-01-13 16:11:23 +0900

    feat: 사용자 정보 저장을 위한 MemberEntity 구현

---
+++ src/main/java/org/bee/metro/core/member/entity/MemberEntity.java
@@ -0,0 +1,35 @@
+package org.bee.metro.core.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.bee.metro.global.entity.BaseEntity;
+
+@Entity
+@Table(name = "MEMBER")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberEntity extends BaseEntity {
+
+    @Id
+    private UUID id;
+
+    private String name;
+
+    private String email;
+
+    private String avatar;
+
+    @Builder
+    public MemberEntity(UUID id, String name, String email, String avatar) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.avatar = avatar;
+    }
+}
