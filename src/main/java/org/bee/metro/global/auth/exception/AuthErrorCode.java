0cdf238e348bfcad4f699edad2dd7d6d82e24e6d
MinSeok Oh <minseok.dev@gmail.com>
2025-01-14 23:47:45 +0900

    feat: 로그인한 사용자 정보를 위한 ArgumentResolver 구현

--- src/main/java/org/bee/metro/global/auth/exception/AuthErrorCode.java
+++ src/main/java/org/bee/metro/global/auth/exception/AuthErrorCode.java
@@ -5,6 +5,7 @@ import org.bee.metro.global.exception.ErrorCode;
 public enum AuthErrorCode implements ErrorCode {
     EXPIRED_TOKEN("A001", "토큰이 만료되었습니다."),
     INVALID_TOKEN("A002", "유효하지 않은 토큰입니다."),
+    UNAUTHORIZED("A003", "권한이 없습니다."),
     ;
 
     private final String code;
---
+++ src/main/java/org/bee/metro/global/auth/resolver/LoginArgumentResolver.java
@@ -0,0 +1,35 @@
+package org.bee.metro.global.auth.resolver;
+
+import org.bee.metro.global.auth.annotation.Login;
+import org.bee.metro.global.auth.exception.AuthErrorCode;
+import org.bee.metro.global.auth.exception.type.AuthenticationException;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean hasLongParameterType = parameter.getParameterType().isAssignableFrom(Long.class);
+        return hasLoginAnnotation && hasLongParameterType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        checkAuthenticated(authentication);
+        return authentication.getPrincipal();
+    }
+
+    private void checkAuthenticated(Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new AuthenticationException("권한이 없습니다.", AuthErrorCode.UNAUTHORIZED);
+        }
+    }
+}
