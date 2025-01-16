30c98a11da16cff42083a1110c48261c397f663d
MinSeok Oh <minseok.dev@gmail.com>
2025-01-16 20:21:28 +0900

    fix: 불필요한 변수 제거

--- src/main/java/org/bee/metro/core/auth/common/OAuthProvider.java
+++ src/main/java/org/bee/metro/core/auth/common/OAuthProvider.java
@@ -1,14 +1,5 @@
 package org.bee.metro.core.auth.common;
 
 public enum OAuthProvider {
-    GOOGLE("google"),
-    NAVER("naver"),
-    KAKAO("kakao")
-    ;
-
-    private final String provider;
-
-    OAuthProvider(String provider) {
-        this.provider = provider;
-    }
+    GOOGLE, NAVER, KAKAO
 }
