7a73a09be34d4d45f62ea50957cfd8f8ba5d0803
MinSeok Oh <minseok.dev@gmail.com>
2025-01-23 23:13:47 +0900

    fix: 레포지토리 내의 Member 반환 메서드를 Optional<Member>로 변경

--- src/main/java/org/bee/metro/core/member/infra/MemberCoreRepository.java
+++ src/main/java/org/bee/metro/core/member/infra/MemberCoreRepository.java
@@ -1,5 +1,6 @@
 package org.bee.metro.core.member.infra;
 
+import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.bee.metro.core.member.domain.Member;
@@ -21,20 +22,14 @@ public class MemberCoreRepository implements MemberRepository {
     }
 
     @Override
-    public Member findByOauthId(String oAuthId) {
-        MemberEntity memberEntity = memberJpaRepository.findByOauthId(oAuthId);
-        if (memberEntity == null) {
-            return null;
-        }
-        return Member.fromEntity(memberEntity);
+    public Optional<Member> findByOauthId(String oAuthId) {
+        Optional<MemberEntity> memberEntity = memberJpaRepository.findByOauthId(oAuthId);
+        return memberEntity.map(Member::fromEntity);
     }
 
     @Override
-    public Member findById(UUID id) {
-        MemberEntity memberEntity = memberJpaRepository.findById(id).orElse(null);
-        if (memberEntity == null) {
-            return null;
-        }
-        return Member.fromEntity(memberEntity);
+    public Optional<Member> findById(UUID id) {
+        Optional<MemberEntity> memberEntity = memberJpaRepository.findById(id);
+        return memberEntity.map(Member::fromEntity);
     }
 }
--- src/main/java/org/bee/metro/core/member/infra/MemberJpaRepository.java
+++ src/main/java/org/bee/metro/core/member/infra/MemberJpaRepository.java
@@ -1,9 +1,10 @@
 package org.bee.metro.core.member.infra;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.bee.metro.core.member.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberJpaRepository extends JpaRepository<MemberEntity, UUID> {
-    MemberEntity findByOauthId(String oAuthId);
+    Optional<MemberEntity> findByOauthId(String oAuthId);
 }
