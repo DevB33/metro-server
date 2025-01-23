ce27733eeaf2b84e12b15bb2beb5618785956608
MinSeok Oh <minseok.dev@gmail.com>
2025-01-23 23:06:21 +0900

    feat: refresh 토큰 확인 및 재발급 로직 구현

--- src/main/java/org/bee/metro/core/auth/application/AuthService.java
+++ src/main/java/org/bee/metro/core/auth/application/AuthService.java
@@ -1,14 +1,17 @@
 package org.bee.metro.core.auth.application;
 
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.bee.metro.core.auth.common.OAuthProvider;
 import org.bee.metro.core.auth.common.OAuthServiceFactory;
 import org.bee.metro.core.auth.dto.MemberCreationPayload;
 import org.bee.metro.core.auth.dto.MemberToken;
+import org.bee.metro.core.auth.exception.AuthErrorCode;
 import org.bee.metro.core.member.application.MemberService;
 import org.bee.metro.core.member.domain.Member;
 import org.bee.metro.global.auth.jwt.AccessTokenProvider;
 import org.bee.metro.global.auth.jwt.RefreshTokenProvider;
+import org.bee.metro.global.exception.type.NotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -40,5 +43,15 @@ public class AuthService {
         String accessToken = accessTokenProvider.generateToken(String.valueOf(member.getId()));
         String refreshToken = refreshTokenProvider.generateToken(String.valueOf(member.getId()));
         return new MemberToken(accessToken, refreshToken);
+    }
+
+    public MemberToken refresh(String refreshToken) {
+        UUID memberId = refreshTokenProvider.parseToken(refreshToken);
+        Member member = memberService.findMemberById(memberId);
+
+        if (member == null) {
+            throw new NotFoundException("사용자를 찾을 수 없습니다.", AuthErrorCode.NOT_FOUND_MEMBER);
+        }
+        return generateMemberToken(member);
     }
 }
--- src/main/java/org/bee/metro/core/auth/exception/AuthErrorCode.java
+++ src/main/java/org/bee/metro/core/auth/exception/AuthErrorCode.java
@@ -8,7 +8,8 @@ public enum AuthErrorCode implements ErrorCode {
     UNAUTHORIZED("A003", "권한이 없습니다."),
     NOT_FOUND_TOKEN("A004", "토큰이 없습니다."),
     TOKEN_FETCH_FAILED("A005", "OAuth 토큰을 가져오는데 실패했습니다."),
-    PROFILE_FETCH_FAILED("A006", "OAuth 프로필을 가져오는데 실패했습니다.")
+    PROFILE_FETCH_FAILED("A006", "OAuth 프로필을 가져오는데 실패했습니다."),
+    NOT_FOUND_MEMBER("A007", "사용자를 찾을 수 없습니다.")
     ;
 
     private final String code;
