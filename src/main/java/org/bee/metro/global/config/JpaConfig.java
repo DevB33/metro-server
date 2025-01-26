cbc2667452a0d32a0441724f7b259e09de31559b
MinSeok Oh <minseok.dev@gmail.com>
2025-01-26 23:47:07 +0900

    fix: EnableJpaAuditing 분리를 위한 JpaConfig 생성

---
+++ src/main/java/org/bee/metro/global/config/JpaConfig.java
@@ -0,0 +1,14 @@
+package org.bee.metro.global.config;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+}
