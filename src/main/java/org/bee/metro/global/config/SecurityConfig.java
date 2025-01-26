970f2a539c7727766a52cdeb536b61f3694a9f88
MinSeok Oh <minseok.dev@gmail.com>
2025-01-26 14:18:32 +0900

    fix: CORS 처리에 대한 책임을 WebMvc -> Security Filter로 이전

--- src/main/java/org/bee/metro/global/config/SecurityConfig.java
+++ src/main/java/org/bee/metro/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.bee.metro.global.config;
 
+import java.util.List;
 import org.bee.metro.global.auth.filter.AuthenticationFilter;
 import org.bee.metro.global.auth.filter.RefreshionFilter;
 import org.bee.metro.global.auth.jwt.AccessTokenProvider;
@@ -17,10 +18,26 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${client.url}")
+    private String allowedOrigins;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AccessTokenProvider jwtProvider) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AccessTokenProvider jwtProvider)
+            throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors
+                        .configurationSource(request -> {
+                            var corsConfig = new org.springframework.web.cors.CorsConfiguration();
+                            corsConfig.setAllowedOrigins(List.of(allowedOrigins));
+                            corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                            corsConfig.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+                            corsConfig.setAllowCredentials(true);
+                            corsConfig.setExposedHeaders(List.of("Custom-Header"));
+                            corsConfig.setMaxAge(3600L);
+                            return corsConfig;
+                        })
+                )
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/login", "/h2-console/**").permitAll()
