eff487139f9453e263fd54c766b4252439797077
MinSeok Oh <minseok.dev@gmail.com>
2025-01-26 23:48:27 +0900

    test: ErrorCode Test를 위한 구현

---
+++ src/test/java/org/bee/metro/document/ErrorCodeDocument.java
@@ -0,0 +1,21 @@
+package org.bee.metro.document;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import org.bee.metro.context.DocumentTest;
+import org.bee.metro.document.errorcode.ErrorCodeFieldsSnippet;
+import org.junit.jupiter.api.Test;
+
+public class ErrorCodeDocument extends DocumentTest {
+
+    @Test
+    void 에러_코드_생성() throws Exception {
+        ErrorCodeFieldsSnippet errorCodeFieldsSnippet = new ErrorCodeFieldsSnippet("error-code", "error-code-template");
+
+        mockMvc.perform(get("/test/error-code"))
+                .andDo(print())
+                .andDo(document("error-code", errorCodeFieldsSnippet));
+    }
+}
