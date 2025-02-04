0f715dc7b9bf1290895ccea16a6cef21d1383ddb
MinSeok Oh <minseok.dev@gmail.com>
2025-02-05 08:29:02 +0900

    feat: 문서 엔티티 생성 및 구현
    - 각 값 중 parentId, ownerId 제외 모두 null이 허용됩니다

---
+++ src/main/java/org/bee/metro/core/document/entity/DocumentEntity.java
@@ -0,0 +1,47 @@
+package org.bee.metro.core.document.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "DOCUMENT")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DocumentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = true)
+    private String title;
+
+    @Column(nullable = true)
+    private String tag;
+
+    @Column(nullable = true)
+    private String cover;
+
+    private UUID parentId;
+
+    private UUID ownerId;
+
+    @Builder
+    public DocumentEntity(UUID id, String title, String tag, String cover, UUID parentId, UUID ownerId) {
+        this.id = id;
+        this.title = title;
+        this.tag = tag;
+        this.cover = cover;
+        this.parentId = parentId;
+        this.ownerId = ownerId;
+    }
+}
