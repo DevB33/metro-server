fc88e18210a0e33769ad3c85b45ebb340e4331e4
MinSeok Oh <minseok.dev@gmail.com>
2025-02-11 20:23:48 +0900

    test: 식별자를 통한 문서 조회 테스트 작성

--- src/test/java/org/bee/metro/core/document/infra/DocumentCoreRepositoryTest.java
+++ src/test/java/org/bee/metro/core/document/infra/DocumentCoreRepositoryTest.java
@@ -1,6 +1,7 @@
 package org.bee.metro.core.document.infra;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -24,7 +25,7 @@ class DocumentCoreRepositoryTest extends RepositoryTest {
     class save_메서드는 {
 
         @Test
-        void 문서를_저장한다() {
+        void 문서를_생성한다() {
             Document document = Document.builder()
                     .id(null)
                     .title("title")
@@ -40,6 +41,74 @@ class DocumentCoreRepositoryTest extends RepositoryTest {
             Document savedDocument = documentRepository.save(document);
 
             assertThat(savedDocument).isNotNull();
+        }
+
+        @Test
+        void 이미_존재하는_문서는_저장한다() {
+            Document document = Document.builder()
+                    .id(null)
+                    .title("title")
+                    .tag("tag")
+                    .icon("icon")
+                    .cover("cover")
+                    .parentId(UUID.randomUUID())
+                    .ownerId(UUID.randomUUID())
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .build();
+
+            Document savedDocument = documentRepository.save(document);
+
+            Document updatedDocument = Document.builder()
+                    .id(savedDocument.getId())
+                    .title("updated title")
+                    .tag("updated tag")
+                    .icon("updated icon")
+                    .cover("updated cover")
+                    .parentId(UUID.randomUUID())
+                    .ownerId(UUID.randomUUID())
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .build();
+
+            Document updatedSavedDocument = documentRepository.save(updatedDocument);
+
+            assertAll(
+                    () -> assertThat(updatedSavedDocument.getTitle()).isEqualTo("updated title"),
+                    () -> assertThat(updatedSavedDocument.getTag()).isEqualTo("updated tag"),
+                    () -> assertThat(updatedSavedDocument.getIcon()).isEqualTo("updated icon"),
+                    () -> assertThat(updatedSavedDocument.getCover()).isEqualTo("updated cover")
+            );
+        }
+    }
+
+    @Nested
+    class findById_메서드는 {
+
+        @Test
+        void 문서를_조회한다() {
+            Document document = Document.builder()
+                    .id(null)
+                    .title("title")
+                    .tag("tag")
+                    .icon("icon")
+                    .cover("cover")
+                    .parentId(UUID.randomUUID())
+                    .ownerId(UUID.randomUUID())
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .build();
+            Document savedDocument = documentRepository.save(document);
+
+            Document foundDocument = documentRepository.findById(savedDocument.getId()).get();
+
+            assertThat(foundDocument).isNotNull();
+        }
+
+        @Test
+        void 문서가_존재하지_않으면_null을_반환한다() {
+            UUID id = UUID.randomUUID();
+            assertThat(documentRepository.findById(id)).isEmpty();
         }
     }
 
