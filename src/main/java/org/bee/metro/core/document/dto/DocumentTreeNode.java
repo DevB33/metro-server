5a836a8eb6a8dc9d416c6e0e62bfe66b26d6bad5
MinSeok Oh <minseok.dev@gmail.com>
2025-02-12 15:29:37 +0900

    feat: DocumentTreeNode 정적 팩토리 메서드 생성

--- src/main/java/org/bee/metro/core/document/dto/DocumentTreeNode.java
+++ src/main/java/org/bee/metro/core/document/dto/DocumentTreeNode.java
@@ -1,7 +1,9 @@
 package org.bee.metro.core.document.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import org.bee.metro.core.document.domain.Document;
 
 public record DocumentTreeNode(
         UUID id,
@@ -9,4 +11,13 @@ public record DocumentTreeNode(
         String icon,
         List<DocumentTreeNode> children
 ) {
+
+    public static DocumentTreeNode createByDocument(Document document) {
+        return new DocumentTreeNode(
+                document.getId(),
+                document.getTitle(),
+                document.getIcon(),
+                new ArrayList<>()
+        );
+    }
 }
