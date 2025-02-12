860cdd290b3c31cb6064ee2c9ce6f0ca7387be0d
MinSeok Oh <minseok.dev@gmail.com>
2025-02-12 19:53:07 +0900

    feat: 문서 삭제 시 권한 확인 로직 추가

--- src/main/java/org/bee/metro/core/document/exception/DocumentErrorCode.java
+++ src/main/java/org/bee/metro/core/document/exception/DocumentErrorCode.java
@@ -3,7 +3,9 @@ package org.bee.metro.core.document.exception;
 import org.bee.metro.global.exception.ErrorCode;
 
 public enum DocumentErrorCode implements ErrorCode {
-    ARGUMENT_IS_NULL("D001", "해당 인자는 null일 수 없습니다")
+    ARGUMENT_IS_NULL("D001", "해당 인자는 null일 수 없습니다"),
+    NOT_FOUND("D002", "해당 문서를 찾을 수 없습니다"),
+    UNAUTHORIZED("D003", "해당 문서에 대한 권한이 없습니다")
     ;
 
     private final String code;
