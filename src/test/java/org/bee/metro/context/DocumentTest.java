a90fd8a44c114212a6a986270280553c7d414acb
MinSeok Oh <minseok.dev@gmail.com>
2025-02-13 10:27:45 +0900

    test: setUpSecurityContext 구현
    - Context에 값을 넣어줍니다

--- src/test/java/org/bee/metro/context/DocumentTest.java
+++ src/test/java/org/bee/metro/context/DocumentTest.java
@@ -1,16 +1,26 @@
 package org.bee.metro.context;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.UUID;
 import org.bee.metro.core.auth.api.AuthApi;
 import org.bee.metro.core.auth.application.AuthService;
 import org.bee.metro.core.document.api.DocumentApi;
 import org.bee.metro.core.document.application.DocumentService;
 import org.bee.metro.document.config.DocumentConfig;
+import org.bee.metro.global.auth.jwt.RefreshTokenProvider;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.TestSecurityContextHolder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,6 +33,9 @@ public abstract class DocumentTest {
     @Autowired
     protected MockMvc mockMvc;
 
+    @Autowired
+    protected RefreshTokenProvider refreshTokenProvider;
+
     @MockitoBean
     protected DocumentService documentService;
 
@@ -31,4 +44,19 @@ public abstract class DocumentTest {
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    protected final UUID randomId = UUID.randomUUID();
+
+    @BeforeEach
+    void setupSecurityContext() {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                randomId,
+                null,
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+        TestSecurityContextHolder.setContext(context);
+    }
 }
