bcc0c83a37d9d8dc4f2d40d7b53a2ef9a4790b34
MinSeok Oh <minseok.dev@gmail.com>
2025-02-15 17:31:27 +0900

    feat: Document 부분 필드 업데이트 로직 구현

--- src/main/java/org/bee/metro/core/document/domain/DocumentRepository.java
+++ src/main/java/org/bee/metro/core/document/domain/DocumentRepository.java
@@ -3,6 +3,7 @@ package org.bee.metro.core.document.domain;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.bee.metro.core.document.common.DocumentFieldType;
 
 public interface DocumentRepository {
     Document save(Document document);
@@ -10,4 +11,5 @@ public interface DocumentRepository {
     List<Document> findByOwnerId(UUID ownerId);
     List<Document> findByParentId(UUID parentId);
     void deleteById(UUID id);
+    void updateField(UUID id, DocumentFieldType type, String value);
 }
