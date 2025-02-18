977373786c471552f1b84b233960e840adfe8e89
MinSeok Oh <minseok.dev@gmail.com>
2025-02-18 21:15:51 +0900

    chore: QNodeEntity 생성

---
+++ src/main/generated/org/bee/metro/core/block/entity/QNodeEntity.java
@@ -0,0 +1,45 @@
+package org.bee.metro.core.block.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QNodeEntity is a Querydsl query type for NodeEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QNodeEntity extends EntityPathBase<NodeEntity> {
+
+    private static final long serialVersionUID = -1279287541L;
+
+    public static final QNodeEntity nodeEntity = new QNodeEntity("nodeEntity");
+
+    public final ComparablePath<java.util.UUID> blockId = createComparable("blockId", java.util.UUID.class);
+
+    public final StringPath content = createString("content");
+
+    public final ComparablePath<java.util.UUID> id = createComparable("id", java.util.UUID.class);
+
+    public final NumberPath<Long> order = createNumber("order", Long.class);
+
+    public final StringPath style = createString("style");
+
+    public QNodeEntity(String variable) {
+        super(NodeEntity.class, forVariable(variable));
+    }
+
+    public QNodeEntity(Path<? extends NodeEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QNodeEntity(PathMetadata metadata) {
+        super(NodeEntity.class, metadata);
+    }
+
+}
+
