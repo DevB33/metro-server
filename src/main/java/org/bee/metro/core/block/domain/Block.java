7b241a4aaf5757b040d2da08a7e54e6ac047a30b
MinSeok Oh <minseok.dev@gmail.com>
2025-02-18 18:45:31 +0900

    feat: Block 기본 도메인 구성

--- src/main/java/org/bee/metro/core/block/domain/Block.java
+++ src/main/java/org/bee/metro/core/block/domain/Block.java
@@ -1,4 +1,20 @@
 package org.bee.metro.core.block.domain;
 
+import java.util.UUID;
+import lombok.Builder;
+
 public class Block {
+
+    private final UUID id;
+    private final BlockType type;
+    private final Long order;
+    private final UUID documentId;
+
+    @Builder
+    public Block(UUID id, BlockType type, Long order, UUID documentId) {
+        this.id = id;
+        this.type = type;
+        this.order = order;
+        this.documentId = documentId;
+    }
 }
