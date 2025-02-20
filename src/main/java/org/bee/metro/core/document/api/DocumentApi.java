1cd8038ba96f01c511cef8b158624c2b5ba0c86a
MinSeok Oh <minseok.dev@gmail.com>
2025-02-20 21:51:36 +0900

    feat: Document의 Tag 수정을 위한 API 구현

--- src/main/java/org/bee/metro/core/document/api/DocumentApi.java
+++ src/main/java/org/bee/metro/core/document/api/DocumentApi.java
@@ -9,6 +9,7 @@ import org.bee.metro.core.document.domain.Document;
 import org.bee.metro.core.document.dto.DetailDocumentPayload;
 import org.bee.metro.core.document.dto.DocumentCreationRequest;
 import org.bee.metro.core.document.dto.DocumentCreationResponse;
+import org.bee.metro.core.document.dto.DocumentTagsUpdateRequest;
 import org.bee.metro.core.document.dto.DocumentTreeNode;
 import org.bee.metro.core.document.dto.DocumentTreeNodeResponse;
 import org.bee.metro.core.document.dto.DocumentUpdateRequest;
@@ -61,6 +62,16 @@ public class DocumentApi {
     ) {
         DetailDocumentPayload document = documentService.findDocumentById(memberId, documentId);
         return ResponseEntity.ok(document);
+    }
+
+    @PatchMapping("/{documentId}/tags")
+    public ResponseEntity<Void> updateDocumentTag(
+            @Login UUID memberId,
+            @PathVariable(value = "documentId") UUID documentId,
+            @RequestBody DocumentTagsUpdateRequest documentTagsUpdateRequest
+    ) {
+        documentService.updateDocumentTags(memberId, documentId, documentTagsUpdateRequest.tags());
+        return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{documentId}/{fieldType}")
