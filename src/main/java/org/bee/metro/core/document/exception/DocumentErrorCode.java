ef2c44f3027b4d64feff1b411bf874297e496aac
MinSeok Oh <minseok.dev@gmail.com>
2025-02-20 20:47:09 +0900

    feat: 데이터베이스 내 Tag 저장을 위한 Converter 생성

--- src/main/java/org/bee/metro/core/document/exception/DocumentErrorCode.java
+++ src/main/java/org/bee/metro/core/document/exception/DocumentErrorCode.java
@@ -6,7 +6,9 @@ public enum DocumentErrorCode implements ErrorCode {
     ARGUMENT_IS_NULL("D001", "해당 인자는 null일 수 없습니다"),
     NOT_FOUND("D002", "해당 문서를 찾을 수 없습니다"),
     UNAUTHORIZED("D003", "해당 문서에 대한 권한이 없습니다"),
-    INVALID_DOCUMENT_FIELD_TYPE("D004", "해당 문서 필드 타입이 올바르지 않습니다")
+    INVALID_DOCUMENT_FIELD_TYPE("D004", "해당 문서 필드 타입이 올바르지 않습니다"),
+    CONVERT_TO_STRING_FAILED("D005", "해당 태그 목록을 문자열로 변경할 수 없습니다"),
+    CONVERT_TO_TAGS_FAILED("D006", "해당 문자열을 태그 목록으로 변경할 수 없습니다")
     ;
 
     private final String code;
