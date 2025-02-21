48c4e789bf8211691b7d63f56044c6d79c24fad5
MinSeok Oh <minseok.dev@gmail.com>
2025-02-21 18:20:46 +0900

    feat: BlockCoreRepository 추가

---
+++ src/main/java/org/bee/metro/core/block/infra/BlockCoreRepository.java
@@ -0,0 +1,22 @@
+package org.bee.metro.core.block.infra;
+
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.bee.metro.core.block.domain.block.Block;
+import org.bee.metro.core.block.domain.block.BlockRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class BlockCoreRepository implements BlockRepository {
+    @Override
+    public Block save(Block block) {
+        return null;
+    }
+
+    @Override
+    public List<Block> findByDocumentId(UUID documentId) {
+        return List.of();
+    }
+}
