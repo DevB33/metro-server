1386ecffa0530b73c3c68fd6b3ddca1442c8663e
MinSeok Oh <minseok.dev@gmail.com>
2025-02-24 22:47:41 +0900

    feat: Document 내의 Block 리스트 조회 메서드 작성

--- src/main/java/org/bee/metro/core/block/infra/BlockJpaRepository.java
+++ src/main/java/org/bee/metro/core/block/infra/BlockJpaRepository.java
@@ -1,8 +1,10 @@
 package org.bee.metro.core.block.infra;
 
+import java.util.List;
 import java.util.UUID;
 import org.bee.metro.core.block.entity.BlockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlockJpaRepository extends JpaRepository<BlockEntity, UUID> {
+    List<BlockEntity> findByDocumentId(UUID documentId);
 }
