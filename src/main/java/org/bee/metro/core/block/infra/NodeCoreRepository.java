91c452e03a611012641473c1d221f22ebf2f2a1a
MinSeok Oh <minseok.dev@gmail.com>
2025-02-24 22:30:18 +0900

    feat: 테스트를 위한 findByBlockId 메서드 작성

--- src/main/java/org/bee/metro/core/block/infra/NodeCoreRepository.java
+++ src/main/java/org/bee/metro/core/block/infra/NodeCoreRepository.java
@@ -1,5 +1,8 @@
 package org.bee.metro.core.block.infra;
 
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.bee.metro.core.block.domain.node.Node;
 import org.bee.metro.core.block.domain.node.NodeRepository;
@@ -17,5 +20,13 @@ public class NodeCoreRepository implements NodeRepository {
         NodeEntity nodeEntity = NodeEntity.from(node);
         NodeEntity savedNodeEntity = nodeJpaRepository.save(nodeEntity);
         return Node.fromEntity(savedNodeEntity);
+    }
+
+    @Override
+    public List<Node> findByBlockId(UUID blockId) {
+        List<NodeEntity> nodeEntityList = nodeJpaRepository.findByBlockId(blockId);
+        return nodeEntityList.stream()
+                .map(Node::fromEntity)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/org/bee/metro/core/block/infra/NodeJpaRepository.java
+++ src/main/java/org/bee/metro/core/block/infra/NodeJpaRepository.java
@@ -1,8 +1,10 @@
 package org.bee.metro.core.block.infra;
 
+import java.util.List;
 import java.util.UUID;
 import org.bee.metro.core.block.entity.NodeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NodeJpaRepository extends JpaRepository<NodeEntity, UUID> {
+    List<NodeEntity> findByBlockId(UUID blockId);
 }
