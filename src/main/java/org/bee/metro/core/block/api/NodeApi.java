7961b75d5df0a3e6dc0cac3f6e95c1fa368f2106
MinSeok Oh <minseok.dev@gmail.com>
2025-03-02 14:48:25 +0900

    feat: Node 순서 변경을 위한 API 구현

--- src/main/java/org/bee/metro/core/block/api/NodeApi.java
+++ src/main/java/org/bee/metro/core/block/api/NodeApi.java
@@ -1,9 +1,14 @@
 package org.bee.metro.core.block.api;
 
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.bee.metro.core.block.application.BlockService;
+import org.bee.metro.core.block.dto.NodeContentUpdateRequest;
 import org.bee.metro.core.block.dto.NodeCreationRequest;
+import org.bee.metro.global.auth.annotation.Login;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +29,20 @@ public class NodeApi {
                 nodeCreationRequest.content(),
                 nodeCreationRequest.order(),
                 nodeCreationRequest.style()
+        );
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{nodeId}/content")
+    public ResponseEntity<Void> updateNodeContent(
+            @Login UUID memberId,
+            @PathVariable(name = "nodeId") UUID nodeId,
+            @RequestBody NodeContentUpdateRequest request
+    ) {
+        blockService.updateNodeContent(
+                nodeId,
+                memberId,
+                request.content()
         );
         return ResponseEntity.ok().build();
     }
