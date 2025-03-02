c9cc60406aef8f89a532d2b29f81382ca48fde26
MinSeok Oh <minseok.dev@gmail.com>
2025-03-02 14:35:17 +0900

    test: Node 생성을 위한 API 테스트 작성

--- src/test/java/org/bee/metro/document/NodeDocument.java
+++ src/test/java/org/bee/metro/document/NodeDocument.java
@@ -1,6 +1,52 @@
 package org.bee.metro.document;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Map;
+import java.util.UUID;
 import org.bee.metro.context.DocumentTest;
+import org.bee.metro.core.block.dto.NodeCreationRequest;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 
 public class NodeDocument extends DocumentTest {
+
+    @Nested
+    class 노드_생성 {
+
+        @Test
+        void 노드_생성에_성공하면_200을_반환한다() throws Exception {
+            NodeCreationRequest nodeCreationRequest = new NodeCreationRequest(
+                    UUID.randomUUID(),
+                    UUID.randomUUID(),
+                    "content",
+                    1L,
+                    Map.of("key", "value")
+            );
+
+            mockMvc.perform(post("/nodes")
+                            .header("Authorization", accessToken)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(nodeCreationRequest)))
+                    .andExpect(status().isOk())
+                    .andDo(document("node/create",
+                            preprocessRequest(prettyPrint()),
+                            requestFields(
+                                    fieldWithPath("blockId").description("블록 ID"),
+                                    fieldWithPath("documentId").description("문서 ID"),
+                                    fieldWithPath("content").description("내용"),
+                                    fieldWithPath("order").description("순서"),
+                                    fieldWithPath("style").description("스타일"),
+                                    fieldWithPath("style.key").description("스타일 키")
+                            )
+                    ));
+        }
+    }
 }
