a8c8c4cbd53e68996b2742f4e5af61468eff95d3
MinSeok Oh <minseok.dev@gmail.com>
2025-03-02 12:06:50 +0900

    feat: Block 생성을 위한 API 구현

---
+++ src/main/java/org/bee/metro/core/block/api/BlockApi.java
@@ -0,0 +1,35 @@
+package org.bee.metro.core.block.api;
+
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.bee.metro.core.block.application.BlockService;
+import org.bee.metro.core.block.domain.block.Block;
+import org.bee.metro.core.block.domain.block.BlockType;
+import org.bee.metro.core.block.dto.BlockCreationRequest;
+import org.bee.metro.core.block.dto.BlockCreationResponse;
+import org.bee.metro.global.auth.annotation.Login;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blocks")
+@RequiredArgsConstructor
+public class BlockApi {
+
+    private final BlockService blockService;
+
+    @PostMapping
+    public ResponseEntity<BlockCreationResponse> createBlock(
+            @Login UUID memberId, BlockCreationRequest blockCreationRequest
+    ) {
+        Block createdBlock = blockService.createBlock(
+                memberId,
+                blockCreationRequest.documentId(),
+                BlockType.valueOf(blockCreationRequest.type().toUpperCase()),
+                blockCreationRequest.upperOrder() + 1
+        );
+        return ResponseEntity.ok(new BlockCreationResponse(createdBlock.getId()));
+    }
+}
